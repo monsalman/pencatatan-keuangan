6d211ecc5cda92829359fb1f39a99c63ba852906
monsalman <salmanfirdaus.m@gmail.com>
2024-10-09 16:27:05 +0700

    redesign DetailTransaksi

--- lib/Page/DetailTransaksi.dart
+++ lib/Page/DetailTransaksi.dart
@@ -34,118 +34,135 @@ class DetailTransaksi extends StatelessWidget {
     bool isIncome = transaction.jenis == 'pemasukan';
     Color transactionColor = isIncome ? Colors.green : Colors.red;
     IconData transactionIcon = isIncome
-        ? CupertinoIcons.arrow_up_right
-        : CupertinoIcons.arrow_down_right;
+        ? CupertinoIcons.arrow_up_right_circle_fill
+        : CupertinoIcons.arrow_down_right_circle_fill;
 
-    // Get current user
     final User? currentUser = supabase.auth.currentUser;
 
     return Scaffold(
-      backgroundColor: WarnaUtama,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 200.0,
-            floating: false,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(transaction.kategori),
-              background: Container(
-                color: transactionColor,
-                child: Center(
-                  child: Icon(
-                    transactionIcon,
-                    size: 80,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+      backgroundColor: Color(0xFF252B48),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              transactionColor.withOpacity(0.8),
+              Color(0xFF2F253D),
+              Color(0xFF252B48),
+            ],
+            stops: [0.0, 0.5, 1.0],
           ),
-          SliverToBoxAdapter(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 250.0,
+              floating: false,
+              pinned: true,
+              backgroundColor: Colors.transparent,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () => Navigator.of(context).pop(),
               ),
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Stack(
+                  fit: StackFit.expand,
                   children: [
-                    Text(
-                      '${isIncome ? '+' : '-'} Rp. ${NumberFormat('#,##0').format(transaction.nilai)}',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: transactionColor,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    _buildDetailItem(Icons.category, 'Kategori', transaction.kategori),
-                    _buildDetailItem(Icons.calendar_today, 'Tanggal', DateFormat('dd MMMM yyyy').format(transaction.tanggal)),
-                    _buildDetailItem(Icons.note, 'Catatan', transaction.catatan),
-                    _buildDetailItem(Icons.account_balance_wallet, 'Jenis', transaction.jenis.toLowerCase()),
-                    if (currentUser != null)
-                      _buildDetailItem(Icons.person, 'Pengguna', currentUser.email ?? 'Unknown'),
-                    if (transaction.imageUrl != null && transaction.imageUrl!.isNotEmpty) ...[
-                      SizedBox(height: 20),
-                      Text(
-                        'Bukti Transaksi',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: WarnaUtama,
+                    Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text(
+                            '${isIncome ? '+' : '-'} ${NumberFormat.currency(locale: 'id', symbol: 'Rp ', decimalDigits: 0).format(transaction.nilai)}',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(height: 10),
-                      FutureBuilder<String>(
-                        future: getSignedUrl(transaction.imageUrl!),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                            return ClipRRect(
-                              borderRadius: BorderRadius.circular(15),
-                              child: Image.network(
-                                snapshot.data!,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  print('Error loading image: $error');
-                                  return Container(
-                                    height: 200,
-                                    color: Colors.grey[300],
-                                    child: Center(
-                                      child: Text('Gagal memuat gambar'),
-                                    ),
-                                  );
-                                },
-                              ),
-                            );
-                          } else {
-                            return CircularProgressIndicator();
-                          }
-                        },
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      right: 20,
+                      child: Icon(
+                        transactionIcon,
+                        size: 80,
+                        color: Colors.white.withOpacity(0.5),
                       ),
-                    ],
+                    ),
                   ],
                 ),
               ),
             ),
-          ),
-        ],
+            SliverToBoxAdapter(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildDetailItem(Icons.category, 'Kategori', transaction.kategori),
+                      _buildDetailItem(Icons.note, 'Catatan', transaction.catatan),
+                      _buildDetailItem(Icons.account_balance_wallet, 'Jenis', transaction.jenis.capitalize()),
+                      _buildDetailItem(Icons.calendar_today, 'Tanggal', DateFormat('dd MMMM yyyy').format(transaction.tanggal)),
+                      if (transaction.imageUrl != null && transaction.imageUrl!.isNotEmpty) ...[
+                        SizedBox(height: 20),
+                        Text(
+                          'Bukti Transaksi',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        _buildImageWidget(transaction.imageUrl!),
+                        SizedBox(height: 20),
+                      ],
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildDetailItem(IconData icon, String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Color(0xFF32374F),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
       child: Row(
         children: [
-          Icon(icon, color: WarnaUtama, size: 24),
-          SizedBox(width: 10),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color(0xFF4E4062),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(icon, color: Colors.white, size: 24),
+          ),
+          SizedBox(width: 15),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -154,7 +171,7 @@ class DetailTransaksi extends StatelessWidget {
                   label,
                   style: TextStyle(
                     fontSize: 14,
-                    color: Colors.grey[600],
+                    color: Colors.grey[400],
                   ),
                 ),
                 SizedBox(height: 4),
@@ -163,13 +180,115 @@ class DetailTransaksi extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    color: WarnaUtama,
+                    color: Colors.white,
                   ),
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildImageWidget(String imageUrl) {
+    return FutureBuilder<String>(
+      future: getSignedUrl(imageUrl),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+          return GestureDetector(
+            onTap: () => _showFullScreenImage(context, snapshot.data!),
+            child: Container(
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  snapshot.data!,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    print('Error loading image: $error');
+                    return Container(
+                      color: Color(0xFF32374F),
+                      child: Center(
+                        child: Text(
+                          'Gagal memuat gambar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          );
+        } else {
+          return Container(
+            height: 200,
+            color: Color(0xFF32374F),
+            child: Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  void _showFullScreenImage(BuildContext context, String imageUrl) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Stack(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: InteractiveViewer(
+                      panEnabled: true,
+                      boundaryMargin: EdgeInsets.all(20),
+                      minScale: 0.5,
+                      maxScale: 4,
+                      child: Image.network(
+                        imageUrl,
+                        fit: BoxFit.contain,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Center(
+                            child: Text(
+                              'Gagal memuat gambar',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  left: 10,
+                  child: IconButton(
+                    icon: Icon(Icons.close, color: Colors.white),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
